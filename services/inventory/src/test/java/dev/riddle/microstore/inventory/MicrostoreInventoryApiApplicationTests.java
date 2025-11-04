01579acd28a198e2db89cd895461cb10b5b7a9c7
Wyatt R <dleprogswe@gmail.com>
2025-11-04 13:30:27 -0500

    fix: update testcontainers dep and fix MicrostoreInventoryApiApplicationTests

--- services/inventory/src/test/java/dev/riddle/microstore/inventory/MicrostoreInventoryApiApplicationTests.java
+++ services/inventory/src/test/java/dev/riddle/microstore/inventory/MicrostoreInventoryApiApplicationTests.java
@@ -2,9 +2,28 @@ package dev.riddle.microstore.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class MicrostoreInventoryApiApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine")
+		.withDatabaseName("inventory-test")
+		.withUsername("test")
+		.withPassword("test");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+		registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
